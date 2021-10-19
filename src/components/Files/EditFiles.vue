de6b0084dd2fb0aae8d6d501013169c094aa30d0
narcosm <narcosm@correo.udistrital.edu.co>
2021-10-19 15:46:54 -0500

    location editfiles

--- src/components/Files/EditFiles.vue
+++ src/components/Files/EditFiles.vue
@@ -381,7 +381,7 @@ export default {
             participants: response.participants,
             format: response.format,
             size: response.size,
-            location: response.location,
+           // location: response.location,
             requierements: response.requierements || "",
             class_learning: response.class_learning,
             type_of_educational_resource: response.type_of_educational_resource,
