9fdbfd1523811534e79f2ccad8617bc9257e6adb
narcosm <narcosm@correo.udistrital.edu.co>
2021-10-19 03:24:49 -0500

    Intalacion y configuracion de chartkickd

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import vuetify from './plugins/vuetify'
 import router from './router'
 import VueFormGenerator from 'vue-form-generator'
 import 'vue-form-generator/dist/vfg.css'
+import Chartkick from 'vue-chartkick'
+import Chart from 'chart.js'
+
+Vue.use(Chartkick.use(Chart))
 
 Vue.use(VueFormGenerator)
 
